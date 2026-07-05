2fc58b046442b41d2456490a3b277ec9867acbaa
michelecostamentoriaofc-web <michelecosta.mentoria.ofc@gmail.com>
2026-07-04 23:34:16 -0300

    Create page.tsx

---
+++ src/app/support/page.tsx
@@ -0,0 +1,8 @@
+export default function SupportPortalPage() {
+  return (
+    <main>
+      <h1>Support Portal</h1>
+      <p>Manage tickets, user requests, technical issues and platform support.</p>
+    </main>
+  );
+}
