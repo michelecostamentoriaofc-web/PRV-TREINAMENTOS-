5752dd4d30229d1c4dd4b700dff815a8c62b495e
michelecostamentoriaofc-web <michelecosta.mentoria.ofc@gmail.com>
2026-07-04 23:33:21 -0300

    Create page.tsx

---
+++ src/app/partner/page.tsx
@@ -0,0 +1,8 @@
+export default function PartnerPortalPage() {
+  return (
+    <main>
+      <h1>Business Partner Portal</h1>
+      <p>Manage partnerships, business opportunities and commercial proposals.</p>
+    </main>
+  );
+}
