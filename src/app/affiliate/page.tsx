ec393f1633e9eb9d378949056e96fc13017d2803
michelecostamentoriaofc-web <michelecosta.mentoria.ofc@gmail.com>
2026-07-04 23:32:22 -0300

    Create page.tsx

---
+++ src/app/affiliate/page.tsx
@@ -0,0 +1,8 @@
+export default function AffiliatePortalPage() {
+  return (
+    <main>
+      <h1>Affiliate Portal</h1>
+      <p>Track affiliate links, sales, commissions and payouts.</p>
+    </main>
+  );
+}
