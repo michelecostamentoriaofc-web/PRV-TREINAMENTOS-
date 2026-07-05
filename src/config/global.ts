1e1165e6cda153e5544b7c31ee9f3b629f65b58c
michelecostamentoriaofc-web <michelecosta.mentoria.ofc@gmail.com>
2026-07-05 16:11:50 -0300

    Create global.ts

---
+++ src/config/global.ts
@@ -0,0 +1,12 @@
+export const countries = [
+  { code: "BR", name: "Brazil", currency: "BRL", language: "pt-BR" },
+  { code: "US", name: "United States", currency: "USD", language: "en-US" },
+  { code: "CA", name: "Canada", currency: "CAD", language: "en-CA" },
+  { code: "GB", name: "United Kingdom", currency: "GBP", language: "en-GB" },
+  { code: "AU", name: "Australia", currency: "AUD", language: "en-AU" },
+  { code: "PT", name: "Portugal", currency: "EUR", language: "pt-PT" }
+];
+
+export const languages = ["pt-BR", "en-US", "en-CA", "en-GB", "en-AU", "pt-PT"];
+
+export const currencies = ["BRL", "USD", "CAD", "GBP", "AUD", "EUR"];
