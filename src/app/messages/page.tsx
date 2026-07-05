3ce8448c5e52794080202ecf3c2cf5f00ddc50a5
michelecostamentoriaofc-web <michelecosta.mentoria.ofc@gmail.com>
2026-07-05 18:13:57 -0300

    Create page.tsx

---
+++ src/app/messages/page.tsx
@@ -0,0 +1,10 @@
+import { DashboardLayout } from "@/components/DashboardLayout";
+
+export default function MessagesPage() {
+  return (
+    <DashboardLayout>
+      <h1>Messages</h1>
+      <p>Internal communication between students, companies, instructors and support.</p>
+    </DashboardLayout>
+  );
+}
