d6656d2edacb856da8126e05388b00706ef32e73
michelecostamentoriaofc-web <michelecosta.mentoria.ofc@gmail.com>
2026-07-05 17:55:30 -0300

    Create owner-actions.ts

---
+++ src/config/owner-actions.ts
@@ -0,0 +1,15 @@
+export const ownerActions = [
+  "Manage all companies",
+  "Manage all users",
+  "Create, edit and delete courses",
+  "Set prices by country and currency",
+  "Manage languages",
+  "Approve instructor partners",
+  "Manage marketplace",
+  "Track global sales",
+  "View full financial reports",
+  "Configure integrations",
+  "Control user permissions",
+  "Manage certificate templates",
+  "Configure entire platform"
+];
