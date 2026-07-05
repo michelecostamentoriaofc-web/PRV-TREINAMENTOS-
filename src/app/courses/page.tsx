daca1afa88cc06cfe4cae5597c1cfc3f0f981bd6
michelecostamentoriaofc-web <michelecosta.mentoria.ofc@gmail.com>
2026-07-04 23:35:19 -0300

    Create page.tsx

---
+++ src/app/courses/page.tsx
@@ -0,0 +1,8 @@
+export default function CoursesPage() {
+  return (
+    <main>
+      <h1>Course Catalog</h1>
+      <p>Browse global training courses by country, language, category and certification.</p>
+    </main>
+  );
+}
