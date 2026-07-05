fbbf15a344de99d5ead5a9538f412a31a4fe36c9
michelecostamentoriaofc-web <michelecosta.mentoria.ofc@gmail.com>
2026-07-05 18:02:38 -0300

    Create page.tsx

---
+++ src/app/users/page.tsx
@@ -0,0 +1,10 @@
+import { DashboardLayout } from "@/components/DashboardLayout";
+
+export default function UsersPage() {
+  return (
+    <DashboardLayout>
+      <h1>Users</h1>
+      <p>Manage global administrators, company admins, instructors, students, affiliates, partners and support users.</p>
+    </DashboardLayout>
+  );
+}
