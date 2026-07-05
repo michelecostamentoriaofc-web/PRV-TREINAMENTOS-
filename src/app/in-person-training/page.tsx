e910e93f585fd602b6030a237f7e51e07a489cd1
michelecostamentoriaofc-web <michelecosta.mentoria.ofc@gmail.com>
2026-07-05 15:53:27 -0300

    Create page.tsx
    export default function InPersonTrainingPage() {
      return (
        <main>
          <h1>In-Person Training</h1>
          <p>Request in-person training in Brazil or internationally with travel and accommodation costs included in the proposal.</p>
        </main>
      );
    }

---
+++ src/app/in-person-training/page.tsx
@@ -0,0 +1,8 @@
+export default function InPersonTrainingPage() {
+  return (
+    <main>
+      <h1>In-Person Training</h1>
+      <p>Request in-person training in Brazil or internationally with travel and accommodation costs included in the proposal.</p>
+    </main>
+  );
+}
