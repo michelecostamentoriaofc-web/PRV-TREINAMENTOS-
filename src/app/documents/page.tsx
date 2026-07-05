6db78ab2e21ce35f789cac540216f20a55901f84
michelecostamentoriaofc-web <michelecosta.mentoria.ofc@gmail.com>
2026-07-05 18:14:21 -0300

    Create page.tsx

---
+++ src/app/documents/page.tsx
@@ -0,0 +1,10 @@
+import { DashboardLayout } from "@/components/DashboardLayout";
+
+export default function DocumentsPage() {
+  return (
+    <DashboardLayout>
+      <h1>Documents</h1>
+      <p>Manage course files, contracts, training records, reports and downloadable materials.</p>
+    </DashboardLayout>
+  );
+}
