6bf55476b69b1c06b56c6852dbde4611e97a7eec
michelecostamentoriaofc-web <michelecosta.mentoria.ofc@gmail.com>
2026-07-05 16:12:39 -0300

    Create course-categories.ts

---
+++ src/config/course-categories.ts
@@ -0,0 +1,12 @@
+export const courseCategories = [
+  "Workplace Safety",
+  "Electrical Safety",
+  "Working at Heights",
+  "Confined Spaces",
+  "Fire Safety",
+  "First Aid",
+  "Machinery and Equipment",
+  "Construction",
+  "Industrial Safety",
+  "Environmental Safety"
+];
