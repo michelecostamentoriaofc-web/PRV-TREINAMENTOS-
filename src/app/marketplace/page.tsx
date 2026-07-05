fd62a7c7b538c9a76ccc8eb90d60c58a3cc6e3f1
michelecostamentoriaofc-web <michelecosta.mentoria.ofc@gmail.com>
2026-07-05 18:04:56 -0300

    Create page.tsx

---
+++ src/app/marketplace/page.tsx
@@ -0,0 +1,10 @@
+import { DashboardLayout } from "@/components/DashboardLayout";
+
+export default function MarketplacePage() {
+  return (
+    <DashboardLayout>
+      <h1>Marketplace</h1>
+      <p>Manage PRV courses, instructor courses, company courses and partner courses.</p>
+    </DashboardLayout>
+  );
+}
