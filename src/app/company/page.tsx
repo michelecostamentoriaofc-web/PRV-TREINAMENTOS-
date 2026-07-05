38a15d277aff574bed254a9c99ed67c0d0edb66a
michelecostamentoriaofc-web <michelecosta.mentoria.ofc@gmail.com>
2026-07-04 23:30:24 -0300

    Create page.tsx

---
+++ src/app/company/page.tsx
@@ -0,0 +1,8 @@
+export default function CompanyPortalPage() {
+  return (
+    <main>
+      <h1>Company Portal</h1>
+      <p>Manage employees, enrollments, certificates and training reports.</p>
+    </main>
+  );
+}
