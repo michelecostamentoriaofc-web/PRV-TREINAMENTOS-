82b5174b8dd2b04629ab63b111a5b464ce4ef31d
michelecostamentoriaofc-web <michelecosta.mentoria.ofc@gmail.com>
2026-07-04 23:36:38 -0300

    Create page.tsx

---
+++ src/app/on-demand-courses/page.tsx
@@ -0,0 +1,8 @@
+export default function OnDemandCoursesPage() {
+  return (
+    <main>
+      <h1>On-Demand Courses</h1>
+      <p>Watch recorded lessons, access materials, take exams and earn certificates.</p>
+    </main>
+  );
+}
