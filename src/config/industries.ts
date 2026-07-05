2243f5378cadbe120f97e3e79a07d2fea0979ab9
michelecostamentoriaofc-web <michelecosta.mentoria.ofc@gmail.com>
2026-07-05 16:39:29 -0300

    Create industries.ts

---
+++ src/config/industries.ts
@@ -0,0 +1,17 @@
+export const industries = [
+  "Construction",
+  "Manufacturing",
+  "Mining",
+  "Oil and Gas",
+  "Energy",
+  "Healthcare",
+  "Logistics",
+  "Agriculture",
+  "Retail",
+  "Hospitality",
+  "Transportation",
+  "Chemical",
+  "Telecommunications",
+  "Education",
+  "Public Sector"
+];
